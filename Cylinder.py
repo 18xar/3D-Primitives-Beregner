33a0bd4359d2925ca117fb44a700030608b4fbd9
Nikolaj J-K <kagenogmig@gmail.com>
2019-05-02 18:31:14 +0200

    formateret koden

--- Cylinder.py
+++ Cylinder.py
@@ -6,12 +6,12 @@ import math
 def CylinderV(V, r, h):
     resultat = 0
     if V == 0:
-        V = math.pi * r**2 * h
+        V = math.pi * r ** 2 * h
         resultat = V
     if r == 0:
         r = math.sqrt(V / (math.pi * h))
         resultat = r
     if h == 0:
-        h = V / (math.pi * r**2)
+        h = V / (math.pi * r ** 2)
         resultat = h
     return resultat
