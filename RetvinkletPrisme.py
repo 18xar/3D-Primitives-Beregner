26f6d68d9ea95da3e89da4ed2316f882776bd130
WilliamT <wtrojel@gmail.com>
2019-05-02 15:05:40 +0200

    Isoleret V og h

--- RetvinkletPrisme.py
+++ RetvinkletPrisme.py
@@ -6,11 +6,15 @@ def RetvinkletPrisme(V, G, h):
     resultat = 0
 
     if V==0:
+        V=G*h
+        resultat=V
 
     if G==0:
         G = V/H
         resultat=G
     if h==0:
+        h=V/G
+        resultat=h
 
     return resultat
 
