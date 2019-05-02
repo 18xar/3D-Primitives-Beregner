bc3f0d0107e84bfeb7e672249f300ba90ed37c8c
Kasper Tonsgaard <kaspertonsgaard@gmail.com>
2019-05-02 15:19:52 +0200

    layout vaiabler

--- main.py
+++ main.py
@@ -4,6 +4,84 @@
 import PySimpleGUI as sg
 
 
+CylenderLayout = [
+  [sg.Text("Udregn Cylender")],
+  [sg.InputText("V"), sg.InputText("r"), sg.InputText("h")],
+  [sg.RButton("Cylender")],
+  [sg.Text("resultat")]
+]
+
+CylenderRoerLayout = [
+  [sg.Text("Udregn Cylender Rør")],
+  [sg.InputText("V"), sg.InputText("R"), sg.InputText("r"), sg.InputText("h")],
+  [sg.RButton("Cylender")],
+  [sg.Text("resultat")]
+]
+
+KasseLayout = [
+  [sg.Text("Udregn Cylender Rør")],
+  [sg.InputText("V"), sg.InputText("l"), sg.InputText("b"), sg.InputText("h")],
+  [sg.RButton("Cylender")],
+  [sg.Text("resultat")]
+]
+
+KegleLayout = [
+  [sg.Text("Udregn Cylender Rør")],
+  [sg.InputText("V"), sg.InputText("r"), sg.InputText("h")],
+  [sg.RButton("Cylender")],
+  [sg.Text("resultat")]
+]
+
+KegleStubLayout = [
+  [sg.Text("Udregn Cylender Rør")],
+  [sg.InputText("V"), sg.InputText("R"), sg.InputText("r"), sg.InputText("h")],
+  [sg.RButton("Cylender")],
+  [sg.Text("resultat")]
+]
+
+KugleLayout = [
+  [sg.Text("Udregn Cylender Rør")],
+  [sg.InputText("V"), sg.InputText("r")],
+  [sg.RButton("Cylender")],
+  [sg.Text("resultat")]
+]
+
+KugleAfsnitLayout = [
+  [sg.Text("Udregn Cylender Rør")],
+  [sg.InputText("V"), sg.InputText("a"), sg.InputText("h")],
+  [sg.RButton("Cylender")],
+  [sg.Text("resultat")]
+]
+
+KugleUdsnitLayout = [
+  [sg.Text("Udregn Cylender Rør")],
+  [sg.InputText("V"), sg.InputText("r"), sg.InputText("h")],
+  [sg.RButton("Cylender")],
+  [sg.Text("resultat")]
+]
+
+PyramideLayout = [
+  [sg.Text("Udregn Cylender Rør")],
+  [sg.InputText("V"), sg.InputText("G"), sg.InputText("h")],
+  [sg.RButton("Cylender")],
+  [sg.Text("resultat")]
+]
+
+PyramideStubLayout = [
+  [sg.Text("Udregn Cylender Rør")],
+  [sg.InputText("V"), sg.InputText("G"), sg.InputText("g"), sg.InputText("h")],
+  [sg.RButton("Cylender")],
+  [sg.Text("resultat")]
+]
+
+RetvinkletPrismeLayout = [
+  [sg.Text("Udregn Cylender Rør")],
+  [sg.InputText("V"), sg.InputText("G"), sg.InputText("h")],
+  [sg.RButton("Cylender")],
+  [sg.Text("resultat")]
+]
+
+
 layout = [
           [sg.RButton("Cylinder"), sg.RButton("CylinderRør")],
           [sg.RButton("RetvinkletPrisme"), sg.RButton("Kasse")],
